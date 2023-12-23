4e96b2962f6d6ae73088565696f3e491308b62c1
HerasimenkoAleksandr <a.gerasimenko.yuz@gmail.com>
2023-12-23 09:42:21 +0200

    dz2

---
+++ Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASP_DZ1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet <Entities.User> User {  get; set; }
+
+        public DataContext (DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("ASP_DZ1");
+        }
+
+    }
+}
---
+++ Data/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace ASP_DZ1.Data.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public DataSetDateTime RegisterDT { get; set; }
+
+    }
+}
